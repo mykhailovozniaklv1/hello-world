7307cb9b66b895dddadc0442cc2d5893883ce2f7
mykhailovozniaklv1 <mykhailovozniak@yahoo.com>
2023-05-02 11:49:49 +0200

    add terraform

---
+++ deploy/terraform/prod/variables.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+  type = string
+  description = "aws region"
+}
+
+variable "aws_profile" {
+  type = string
+  description = "aws profile"
+}
+
+variable "instance_name" {
+  type = string
+  description = "instance name"
+}
+
+variable "instance_type" {
+  type = string
+  description = "instance type"
+}
+
+variable "instance_ami" {
+  type = string
+  description = "instance ami PRODUCTION"
+}
